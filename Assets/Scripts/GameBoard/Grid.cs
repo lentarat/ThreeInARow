f055d61484f9d06b0433c1e692aa11aedbb9a53f
lentar <lntr.exe@gmail.com>
2023-08-06 21:01:59 +0300

    grid

--- Assets/Scripts/GameBoard/Grid.cs
+++ Assets/Scripts/GameBoard/Grid.cs
@@ -6,16 +6,91 @@ public class Grid : MonoBehaviour
 {
     private enum FigureType
     {
-        Normal,
+        Octagon,
+        Pentagon,
+        Triangle,
+        Quad,
+        Circle,
         Count
     }
 
     [System.Serializable]
     private struct FigurePrefab
     {
-        private FigureType type;
-        private GameObject prefab;
+        public FigureType Type;
+        public GameObject Prefab;
     }
 
-    public 
+    [Header("Grid Size")]
+    [SerializeField] private int _xDim;
+    [SerializeField] private int _yDim;
+
+    private Dictionary<FigureType, GameObject> _piecePrefabDictionary = new Dictionary<FigureType, GameObject>();
+    [Header("Figure & Background Cell Prefabs")]
+    [SerializeField] private FigurePrefab[] _figurePrefabs;
+    [SerializeField] private GameObject _backgroundCellPrefab;
+
+    private GameObject[,] _figures;
+    private Vector2 _centeredGridInWorldPosition;
+
+    private void Awake()
+    {
+        CenterTheGrid();
+        CopyFigureArrayToDictionary();
+        InitializeGridBackgroundCells();
+        InitializeFigures();
+    }
+
+    private void CenterTheGrid()
+    {
+        _centeredGridInWorldPosition = new Vector2(transform.position.x - _xDim / 2f + 0.5f, transform.position.y - _yDim / 2f + 0.5f);
+    }
+
+    private void CopyFigureArrayToDictionary() 
+    {
+        for (int i = 0; i < _figurePrefabs.Length; i++)
+        {
+            if (_piecePrefabDictionary.ContainsKey(_figurePrefabs[i].Type) == false)
+            {
+                _piecePrefabDictionary.Add(_figurePrefabs[i].Type, _figurePrefabs[i].Prefab);
+            }
+        }
+    }
+
+    private void InitializeGridBackgroundCells()
+    {
+        for (int x = 0; x < _xDim; x++)
+        {
+            for (int y = 0; y < _yDim; y++)
+            {
+                Vector2 backgroundCellOffset = new Vector3(x, y);
+                Instantiate(_backgroundCellPrefab, _centeredGridInWorldPosition + backgroundCellOffset, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    private void InitializeFigures()
+    {
+        _figures = new GameObject[_xDim, _yDim];
+
+        for (int x = 0; x < _xDim; x++)
+        {
+            for (int y = 0; y < _yDim; y++)
+            {
+                Vector2 figureOffset = new Vector3(x, y);
+
+                GameObject randomFigureToBeInstantiated;
+                _piecePrefabDictionary.TryGetValue( GetRandomFigureType(), out randomFigureToBeInstantiated);
+
+                _figures [x, y] = Instantiate(randomFigureToBeInstantiated, _centeredGridInWorldPosition + figureOffset, Quaternion.identity, transform);
+                _figures[x, y].name = $"Figure at x:{x} y:{y}"; 
+            }
+        }
+    }
+
+    private FigureType GetRandomFigureType()
+    {
+        int randomFigureTypeNumber = Random.Range(0, (int)FigureType.Count);
+        return (FigureType)randomFigureTypeNumber;
+    }
 }
